d2583dfe218ec0d9468f4c162a4ba999f6b042fd
Rafael Uchôa <113214241+uuchoaa@users.noreply.github.com>
2023-04-28 21:12:36 +0000

    Generates repositories

--- lib/generators/crud/crud_generator.rb
+++ lib/generators/crud/crud_generator.rb
@@ -1,4 +1,3 @@
-# require 'rails/generators/named_base'
 
 class CrudGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('templates', __dir__)
@@ -7,18 +6,13 @@ class CrudGenerator < Rails::Generators::NamedBase
 
   def create_domain_file
     template "domain_entity.rb.tt", File.join("app/domain/entities", class_path, "#{file_name}.rb")
+    template "domain_repository_interface.rb.tt", File.join("app/domain/repositories", class_path, "#{file_name}_repository.rb")
   end
 
-#   def create_application_files
-#     template "application_create_use_case.rb.tt", File.join("app/application/use_cases", class_path, "create_#{file_name}.rb")
-#     template "application_list_use_case.rb.tt", File.join("app/application/use_cases", class_path, "list_#{plural_file_name}.rb")
-#     template "application_update_use_case.rb.tt", File.join("app/application/use_cases", class_path, "update_#{file_name}.rb")
-#     template "application_delete_use_case.rb.tt", File.join("app/application/use_cases", class_path, "delete_#{file_name}.rb")
-#   end
-
-#   def create_infrastructure_files
-#     template "infrastructure_repository.rb.tt", File.join("app/infrastructure/repositories", class_path, "#{file_name}_repository.rb")
-#   end
+  def create_infrastructure_files
+    generate "model", "#{file_name}_record #{attributes.map{|a| "#{a.name}:#{a.type}" }.join(' ')} --no-test-framework"
+    template "infrastructure_repository.rb.tt", File.join("app/infrastructure/repositories/active_record", class_path, "#{file_name}_repository.rb")
+  end
 
 #   def create_adapter_files
 #     template "adapter_controller.rb.tt", File.join("app/controllers", class_path, "#{plural_file_name}_controller.rb")
