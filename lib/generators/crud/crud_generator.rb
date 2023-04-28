7b92a8bfff33e7fb528c4f88cecf317c6263ce54
Rafael Uchôa <113214241+uuchoaa@users.noreply.github.com>
2023-04-28 21:45:16 +0000

    Generates the application use cases too

--- lib/generators/crud/crud_generator.rb
+++ lib/generators/crud/crud_generator.rb
@@ -4,6 +4,10 @@ class CrudGenerator < Rails::Generators::NamedBase
 
   argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
+#   class_option :actions, type: :array, default: %w[list create show edit update destroy], desc: "Select specific actions to generate (list, create, show, edit, update destroy)"
+  class_option :actions, type: :array, default: %w[list], desc: "Select specific actions to generate (list, create, show, edit, update destroy)"
+
+
   def create_domain_file
     template "domain_entity.rb.tt", File.join("app/domain/entities", class_path, "#{file_name}.rb")
     template "domain_repository_interface.rb.tt", File.join("app/domain/repositories", class_path, "#{file_name}_repository.rb")
@@ -14,7 +18,20 @@ class CrudGenerator < Rails::Generators::NamedBase
     template "infrastructure_repository.rb.tt", File.join("app/infrastructure/repositories/active_record", class_path, "#{file_name}_repository.rb")
   end
 
+  def create_application_files
+    allowed_actions.each do |action|
+      template "application_use_cases/#{action}_use_case.rb.tt", File.join("app/application/use_cases", class_path, "#{action}_#{plural_file_name}.rb")
+    end
+  end
+
 #   def create_adapter_files
 #     template "adapter_controller.rb.tt", File.join("app/controllers", class_path, "#{plural_file_name}_controller.rb")
 #   end
+
+  private 
+
+  def allowed_actions
+    options[:actions].any? ? options[:actions] : %w[list create show edit update destroy]
+  end
+  
 end
